ad85689e26e5b9a59cab0ee2eeee64ec72787bb7
{haptism} <haptism007@gmail.com>
2019-04-11 19:28:57 +0900

    ;;
    ''

--- src/Day02_7.java
+++ src/Day02_7.java
@@ -4,24 +4,43 @@ public class Day02_7 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+
+		System.out.println("암호화 숫자를 입력하세요");
+		int num = sc.nextInt();
+
 		System.out.println("영어와 한글자를 입력하세요");
 		String str = sc.next();
 		char ch = str.charAt(0);
 
+		System.out.println(ch);
+
 		// 입력된 글자가 대문자 인지, 소문자 인지, 알파벳이 아닙니다.
 
-		if (ch >= 97 && ch <= 122) {
-			System.out.println(ch + "는 소문자");
-		} else if (ch >= 65 && ch <= 90) {
-			System.out.println(ch + "는 대문자");
+		int change = ch + num; // ch가 int타입으로 자동형변환
+		char change1 = (char) change; // 암호화한 문자를 char로 형변환
+		// ch = (char)(ch+num);
+
+		if (ch >= 65 && ch <= 90) {
+			// System.out.println("대문자");
+			if (change1 >= 90)
+				change1 -= 26;
+			else if (change1 < 65)
+				change1 += 26;
+			
+		} else if (ch >= 97 && ch <= 122) {
+			// System.out.println("소문자");
+			if (change1 >= 122)
+				change1 -= 26;
+			else if (change1 < 97)
+				change1 += 26;
 		} else {
-			System.out.println(ch + "는 알파벳이 아닙니다");
+			System.out.println("알파벳이 아님");
 		}
+		System.out.println(change1);
 
 		// 아스키 코드를 이용한 예제
 		// 숫자를 문자로
 		// 문자를 숫자로
 
 	}
-
 }
